c768da3b8ba04e821cb8f4ec273ea665828badfb
cjwebb <colinjameswebb@hotmail.com>
2016-04-06 21:05:28 +0100

    Return some data from sqlite for API calls

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var fs = require('fs');
-
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('../db.sqlite3');
 var app = express();
+var _ = require('lodash');
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
@@ -18,6 +21,16 @@ app.get('/', function(req, res) {
       { name: "Charlie", age: 26 }
     ]
     res.render('home', { elm: data, payload: JSON.stringify(payload) });
+  });
+});
+
+app.get('/api/users/colin', function(req, res) {
+  db.all("SELECT * FROM data", function(err, rows){
+    if (err) return res.json("oh noes");
+    res.json(_.map(rows, function(x){
+      x.data = JSON.parse(x.data);
+      return x;
+    }));
   });
 });
 
