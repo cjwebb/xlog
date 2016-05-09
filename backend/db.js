9496e29688b6b387f3c79e9ed2255bd463a2b25f
cjwebb <colinjameswebb@hotmail.com>
2016-05-09 17:44:45 +0100

    Add raw data api endpoints. No validation yet.

--- backend/db.js
+++ backend/db.js
@@ -3,9 +3,11 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('../db.sqlite3');
 var _ = require('lodash');
+var uuid = require('node-uuid');
+var moment = require('moment');
 
 module.exports.initializeDatabase = function(callback) {
-  db.run("CREATE TABLE IF NOT EXISTS data (date DATETIME, name TEXT, data TEXT)", callback);
+  db.run("CREATE TABLE IF NOT EXISTS data (id TEXT, timestamp DATETIME, username TEXT, dataname TEXT, data TEXT)", callback);
 };
 
 module.exports.getUserLog = function(username, callback) {
@@ -14,6 +16,40 @@ module.exports.getUserLog = function(username, callback) {
 
     var data = _.map(rows, function(x) {
       x.data = JSON.parse(x.data);
+      return x;
+    });
+    return callback(null, data);
+  });
+};
+
+module.exports.insertRaw = function(username, dataname, data, callback) {
+  var id = uuid.v1();
+  var timestamp = moment().toISOString();
+  var sql = "INSERT INTO data (id, timestamp, username, dataname, data) VALUES ($id, $timestamp, $username, $dataname, $data)";
+  var params = {
+    $id: id,
+    $timestamp: timestamp,
+    $username: username,
+    $dataname: dataname,
+    $data: JSON.stringify(data)
+  };
+  return db.run(sql, params, callback);
+};
+
+module.exports.getRaw = function(username, dataname, callback) {
+  var sql = "SELECT * FROM data WHERE username = $username AND dataname = $dataname ORDER BY timestamp DESC, id DESC";
+  // todo - implement order by column by munging v1 uuids: https://github.com/broofa/node-uuid/issues/75
+  var params = {
+    $username: username,
+    $dataname: dataname
+  };
+  return db.all(sql, params, function(err, rows){
+    if (err) return callback(err);
+
+    var data = _.map(rows, function(x) {
+      x.data = JSON.parse(x.data);
+      delete x.username;
+      delete x.dataname;
       return x;
     });
     return callback(null, data);
--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,12 @@
 
 var express = require('express');
 var exphbs = require('express-handlebars');
-var app = express();
+var bodyParser = require('body-parser');
 var gzip = require('compression');
 var jwt = require('express-jwt');
 var db = require('./db.js');
 var config = require('./config.js');
+var app = express();
 
 var jwtCheck = jwt({
   secret: new Buffer(config.authSecret, 'base64'),
@@ -14,6 +15,7 @@ var jwtCheck = jwt({
 });
 
 app.use(gzip());
+app.use(bodyParser.json());
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.use('/static', express.static(__dirname + '/../build'));
@@ -51,6 +53,32 @@ app.get('/api/users/:name', function(req, res) {
     }
     res.json(data);
   });
+});
+
+app.get('/api/:username/:dataname/raw', function(req, res) {
+  // todo - santize :username and :dataname
+  // todo - check permissions for username (via middleware)
+  db.getRaw(req.params.username, req.params.dataname, function(err, data) {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ error: 'DatabaseError' });
+    }
+    return res.json({raw: data});
+  });
+});
+
+app.post('/api/:username/:dataname/raw', function(req, res) {
+  if (req.body) {
+    db.insertRaw(req.params.username, req.params.dataname, req.body, function(err) {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'DatabaseError' });
+      }
+      return res.end();
+    });
+  } else {
+    return res.status(400).json({ error: 'Missing data' });
+  }
 });
 
 // ---- Initialise App ----
